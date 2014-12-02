2c8912f9e27d614378153114aaff5eb6f445b232
Aetf <7437103@gmail.com>
2014-12-02 15:50:52 +0800

    Test: use zip for data

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -1,4 +1,7 @@
 #! /bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd ${DIR}
+rm -rf data/libtorrent data/uTorrent 2>&1 > /dev/null
+unzip data/libtorrent.zip -d data > /dev/null
+unzip data/uTorrent.zip -d data > /dev/null
 LD_LIBRARY_PATH=${DIR}/../src:/home/aetf/lib ${DIR}/tests 1>&2
